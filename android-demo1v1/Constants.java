86d2586319145effd0b712770a692ecb41962489
AndroidDird <15901533520@163.com>
2018-12-14 17:02:01 +0800

    传错路径了
    传错路径了

--- android-demo1v1/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.shinevv.vvroom.demo1v1;
-
-public class Constants {
-    // 演示服务器，socket.io, 提供通讯录和成员列表功能
-    public static final String DEMO_SERVER_URL = "https://58.87.67.88:3001";
-}
